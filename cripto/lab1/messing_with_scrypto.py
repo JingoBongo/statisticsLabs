a5a4a2e389814270fda4b2d249a50696bc818ed0
JingoBongo <https://github.com/JingoBongo>
2022-02-06 20:33:06 +0200

    removed cripto because it was moved to distinct repository

--- cripto/lab1/messing_with_scrypto.py
+++
@@ -1,7 +0,0 @@
-import scrypt
-
-data = scrypt.encrypt('a secret message', 'password')
-
-
-
-print(scrypt.decrypt(data, 'password'))
--- cripto/lab1/messing_with_str_len.py
+++
@@ -1,6 +0,0 @@
-sstr = '1234'
-
-if len(sstr) % 2 == 1:
-    print('odd')
-else:
-    print('even')
